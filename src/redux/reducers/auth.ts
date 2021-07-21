f126855bc6abbd4cbd7d06ad16659cbf5398b805
Joe Ballantyne <josephpballantyne@gmail.com>
2021-07-21 22:12:42 +0800

    persist login

--- src/redux/reducers/auth.ts
+++ src/redux/reducers/auth.ts
@@ -6,21 +6,28 @@ interface Auth {
   username: string | null;
 }
 
+const refreshedAuth = window.localStorage.getItem('authenticated');
+const refreshedUser = window.localStorage.getItem('username');
+
 const initialState: Auth = {
-  authenticated: false,
-  username: null,
+  authenticated: !!refreshedAuth,
+  username: refreshedUser || null,
 };
 
 const auth = function (state = initialState, action: AnyAction) {
   switch (action.type) {
     case LOGIN: {
       const { username } = action.payload;
+      window.localStorage.setItem('authenticated', 'true');
+      window.localStorage.setItem('username', username);
       return {
         authenticated: true,
         username,
       };
     }
     case LOGOUT: {
+      window.localStorage.removeItem('authenticated');
+      window.localStorage.removeItem('username');
       return {
         authenticated: false,
         username: null,
